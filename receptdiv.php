1cbaa9af9419e440d112f11acfed7d97ea7df894
dzhemaptula <dzhem.aptula@gmail.com>
2017-02-22 19:48:10 +0100

    Recept open div goed geordend, ingredienten rechts enz.

--- receptdiv.php
+++ receptdiv.php
@@ -1,22 +1,25 @@
 <div style="overflow-y: scroll;" class="receptdiv" id="rec<?= $xrec ?>"><!--dit is waar wij divs genereren bij het clicken op recepten-->
-    <b><a id="titelreceptdiv"><?=htmlspecialchars($row['naam'], ENT_QUOTES, 'UTF-8');?></a></b>
-    <?php
-    $idvanrecept = $row['id'];
-    for($x=0;$x<5;$x++){
-        echo '<div onclick="ster'.$x.$xrec.'();" id="'.$x.$xrec.'ster" class="ratezelf">
+    <div class="divrecepttitel"><b><a id="titelreceptdiv"><?=htmlspecialchars($row['naam'], ENT_QUOTES, 'UTF-8');?></a></b></div>
+    <div class="ratester">
+        <?php
+        $idvanrecept = $row['id'];
+        for($x=0;$x<5;$x++){
+            echo '<div onclick="ster'.$x.$xrec.'();" id="'.$x.$xrec.'ster" class="ratezelf">
                     <img onclick="ster'.$x.'();" width="30px" height="30px"		
 	src="http://icons.iconarchive.com/icons/icons8/christmas-flat-color/256/star-icon.png"></div>';
-        echo '<script>function ster'.$x.$xrec.'(){';
-        for($y=0;$y<$x+1;$y++){
-            echo '
+            echo '<script>function ster'.$x.$xrec.'(){';
+            for($y=0;$y<$x+1;$y++){
+                echo '
 		document.getElementById("'.$y.$xrec.'ster").style.opacity = "1";';
-        }
-        for($y=$x+1;$y<6;$y++){
-            echo '
+            }
+            for($y=$x+1;$y<6;$y++){
+                echo '
 		document.getElementById("'.$y.$xrec.'ster").style.opacity = "0.3";';
-        }
-        echo "}</script>";
-    }?>
+            }
+            echo "}</script>";
+        }?>
+    </div>
+
     <script>
         function confirmationDelete(anchor)
         {
